badf1c5c5831f1af7fea21786d670d2781a66098
Thiago Matos <thiagoematos@gmail.com>
2017-12-16 13:19:55 -0400

    Estudo de currying, Factory Function, Flat Map usando reduce

---
+++ pokemon.js
@@ -0,0 +1,25 @@
+// Factory Function
+const Treinador = (nome, pokemons) => { return { nome, pokemons } }
+const Pokemon = (nome, tipo) => { return { nome, tipo } }
+
+const eletrico = 'Elétrico'
+const planta = 'Planta'
+const psiquico = 'Psíquico'
+
+const treinadores = [
+    Treinador('Ash', [Pokemon('Pikachu', eletrico), Pokemon('Bulbassaur', planta)]),
+    Treinador('Misty', [Pokemon('Psyduck', psiquico)]),
+    Treinador('Brock', [Pokemon('Newtow', psiquico)])
+]
+
+// Flat Map
+const pokemons =
+    treinadores
+        .map(treinador => treinador.pokemons)
+        .reduce((pokemons, pokemon) => [...pokemons, ...pokemon], []); // Destructuring
+console.log(pokemons)
+
+// Currying
+const porTipo = tipo => pokemon => tipo === pokemon.tipo
+const pokemonsPsiquicos = pokemons.filter(porTipo(psiquico))
+console.log(pokemonsPsiquicos)
