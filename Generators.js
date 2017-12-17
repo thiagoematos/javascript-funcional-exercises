3e94297d4ae1402ccbcc00a7b5f710aa3accb5d1
Thiago Matos <thiagoematos@gmail.com>
2017-12-17 16:23:51 -0400

    Outras formas de iterar num generator

--- Generators.js
+++ Generators.js
@@ -4,6 +4,7 @@ const generator = function* () {
     yield 4
 }
 
+// Generators com função especial de leitura
 const generatorRunner = (generator, accumulator = []) => {
     const next = generator.next()
     if (next.done) {
@@ -14,3 +15,20 @@ const generatorRunner = (generator, accumulator = []) => {
 
 const instanceofGenerator = generator()
 console.log(generatorRunner(instanceofGenerator))
+
+const fibonacciGenerator = function* (numberOfItems) {
+    let pre = 0, cur = 1
+    while (numberOfItems-- > 0) {
+        [pre, cur] = [cur, pre + cur]
+        yield cur
+    }
+}
+
+// Com for
+for (let item of fibonacciGenerator(10)) {
+    console.log(item)
+}
+
+// Com destructuring
+const numeros = [...fibonacciGenerator(8)]
+console.log(numeros)
